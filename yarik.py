30b421e925f863633aac69ff9294cfc05bfdb7bf
YarIk-TmX <165916380+YarIk-TmX@users.noreply.github.com>
2024-04-10 17:25:23 +0300

    Update yarik.py

--- yarik.py
+++ yarik.py
@@ -1,8 +1,8 @@
-from PyQt5.QtWidgets import QPushButton, QApplication, QWidget, QLabel, QListWidget, QVBoxLayout, QHBoxLayout, QFileDialog
-from PyQt5.QtGui import QPixmap
-from PIL import Image
-import os
-from PIL import ImageFilter
+from PyQt5.QtWidgets import QPushButton, QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout
+from photoshop import win2, app2
+from victorina import main_window, app3
+
+
 #Создание окна
 app =QApplication([])
 win = QWidget()
@@ -14,10 +14,17 @@ foto_btn = QPushButton('Фотошоп')
 wic_btn = QPushButton('Викторина')
 
 
+
 main_win = QHBoxLayout()
 winn = QVBoxLayout()
 winn.addWidget(x1)
 
+def photoshop():
+    win2.show()
+    app2.exec_()
+def victorina():
+    main_window.show()
+    app3.exec_()
 
 
 winn.addLayout(main_win)
@@ -26,13 +33,11 @@ main_win.addWidget(wic_btn)
 
 win.setLayout(winn)
 
+foto_btn.clicked.connect(photoshop)
+wic_btn.clicked.connect(victorina)
 
 
-
-
-
-
-
+    
 
 
 
