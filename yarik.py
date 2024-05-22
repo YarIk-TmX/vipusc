05b4de97ae6b7b602afc7d6d14ff37bee6418283
Ярик голубев <165916380+YarIk-TmX@users.noreply.github.com>
2024-05-22 16:33:43 +0300

    Update yarik.py

--- yarik.py
+++ yarik.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QPushButton, QApplication, QWidget, QLabel, QVBoxLay
 from photoshop import win2, app2
 from victorina import main_window, app3
 from chislo import win4, app4
+from clicker import clicker
 #Создание окна
 app =QApplication([])
 win = QWidget()
@@ -12,6 +13,7 @@ x1 = QLabel('           Выбери что вам нужно:')
 foto_btn = QPushButton('Фотошоп')
 wic_btn = QPushButton('Викторина')
 chslo_btn = QPushButton('Угадай число')
+clicker_btn = QPushButton('Кликер')
 
 
 main_win = QHBoxLayout()
@@ -28,18 +30,21 @@ def chislo():
     win4.show()
     app4.exec_()
 
+def clicker1():
+    clicker()
 
 winn.addLayout(main_win)
 main_win.addWidget(foto_btn)
 main_win.addWidget(wic_btn)
 main_win.addWidget(chslo_btn)
+main_win.addWidget(clicker_btn)
 
 win.setLayout(winn)
 
 foto_btn.clicked.connect(photoshop)
 wic_btn.clicked.connect(victorina)
 chslo_btn.clicked.connect(chislo)
-
+clicker_btn.clicked.connect(clicker1)
 
     
 
@@ -60,10 +65,3 @@ app.exec_()
 
 
 
-
-
-
-
-
-win.show()
-app.exec_()
